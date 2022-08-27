9158d3b644a96b3f759f4ca6a18cb19709a05415
Roman Lesnoy <romanlesnoy@outlook.com>
2022-08-27 18:55:39 +0300

    Add Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <>
+            <h1 className="title">Anoter To-Do List</h1>
+        </>
+    );
+};
+
+export default Header;
